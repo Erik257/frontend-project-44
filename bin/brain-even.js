9d69280244f58fd99a9cad478e3ab3637507a3fb
Erik257 <erikdzhanzian06@gmail.com>
2023-03-02 11:47:03 +0300

    redid mistakes in step 5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import welcome from '../src/cli.js';
 
-const welcome = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-};
+welcome()
  
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -14,14 +11,13 @@ const getRandomNumber = (min, max) => {
 const isEven = (num) => num % 2 === 0 ? 'yes' : 'no'
 
 const game = () => {
-    console.log (welcome());
     console.log ('Answer "yes" if the number is even, otherwise answer "no".')
+    for (let i = 0; i < 3; i += 1) {
     const num = getRandomNumber(1, 101);
     const qustion = isEven(num)
     console.log (`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ')
-
-
+      }
 }
 
 game();
