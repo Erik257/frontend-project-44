295868e38a899e086293025ac7d04add16e09bf1
Erik257 <erikdzhanzian06@gmail.com>
2023-03-03 11:41:36 +0300

    finished the first game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,22 +2,33 @@
 import readlineSync from 'readline-sync';
 import welcome from '../src/cli.js';
 
-welcome()
+
  
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const isEven = (num) => num % 2 === 0 ? 'yes' : 'no'
+const name = welcome()
 
 const game = () => {
     console.log ('Answer "yes" if the number is even, otherwise answer "no".')
     for (let i = 0; i < 3; i += 1) {
     const num = getRandomNumber(1, 101);
-    const qustion = isEven(num)
+    const isEven = num % 2 === 0 ? 'yes' : 'no'
+    const qustion = num
     console.log (`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ')
-      }
+    if (isEven === answer) {
+        console.log ('Correct!')
+    } else {
+        console.log (`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.`)
+        console.log(`Let's try again, ${name}!`)
+        break
+    }
+    if (i === 2) {
+        console.log(`Congratulations, ${name}!`)
+    }
+}
 }
 
 game();
