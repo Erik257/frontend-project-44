4835c15978e5f66a954dd67f58f9fb35d95a861c
Erik257 <erikdzhanzian06@gmail.com>
2023-03-03 14:05:21 +0300

    finished the second game and created a common logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,32 +1,19 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import welcome from '../src/cli.js';
+import getRandomNumber from '../src/randomnumber.js';
+import logic from '../src/index.js';
 
-
- 
-const getRandomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-const name = welcome()
 
 const game = () => {
     console.log ('Answer "yes" if the number is even, otherwise answer "no".')
     for (let i = 0; i < 3; i += 1) {
     const num = getRandomNumber(1, 101);
-    const isEven = num % 2 === 0 ? 'yes' : 'no'
-    const qustion = num
+    const trueanswer = num % 2 === 0 ? 'yes' : 'no'
     console.log (`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ')
-    if (isEven === answer) {
-        console.log ('Correct!')
-    } else {
-        console.log (`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.`)
-        console.log(`Let's try again, ${name}!`)
+    const ural = logic(trueanswer, answer, i);
+    if (ural) {
         break
-    }
-    if (i === 2) {
-        console.log(`Congratulations, ${name}!`)
     }
 }
 }
